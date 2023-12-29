f4ab2ab8984bb2757b52b757b9a5254f1bc22202
Ronit <ronit@siva.sh>
2023-12-30 01:38:18 +0900

    fix: isbot

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,9 @@ module.exports = {
   defaultCommandTimeout: 20000,
   e2e: {
     setupNodeEvents(on, config) {
-      config.baseUrl = process.env.CYPRESS_BASE_URL ?? "http://localhost:8788/";
-      return config;
+      config.baseUrl = process.env.CYPRESS_BASE_URL ?? 'http://localhost:3000'
+      return config
     },
     supportFile: false,
   },
-};
+}
